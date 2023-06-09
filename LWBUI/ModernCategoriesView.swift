37c67b8f6456f91fdc66d7baaed9b1e51e9266cc
wonbeen <wb277@naver.com>
2023-06-09 14:06:13 +0900

    feat : "ModernCategoriesView" 추가

---
+++ LWBUI/ModernCategoriesView.swift
@@ -0,0 +1,43 @@
+//
+//  ModernCategoriesView.swift
+//  LWBUI
+//
+//  Created by 이원빈 on 2023/05/26.
+//
+
+import SwiftUI
+
+struct ModernCategoriesView: View {
+    let data: [String]
+    @Binding var selectedCategory: String
+    
+    var body: some View {
+        VStack {
+            HorizontalScrollBar(
+                data: data,
+                selectedCategory: $selectedCategory
+            )
+            .frame(width: UIScreen.main.bounds.width)
+            
+            
+            TabView(selection: $selectedCategory) {
+                ForEach(data, id: \.self) { number in
+                    ListView(data: number)
+                        .tag(number)
+                }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+//            .animation(.default, value: selectedCategory)
+        }
+    }
+}
+
+struct ModernCategoriesView_Previews: PreviewProvider {
+    @State static var cate = "C View"
+    static var previews: some View {
+        ModernCategoriesView(
+            data: ["A is a category of View", "B is also a catego", "C View", "D end View1"],
+            selectedCategory: $cate
+        )
+    }
+}
