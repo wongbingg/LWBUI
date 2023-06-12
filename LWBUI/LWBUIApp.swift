1ef6724394d4c8df238d68a2cf7a8a5b52a18995
wonbeen <wb277@naver.com>
2023-06-12 16:55:57 +0900

    feat : "LWBUIApp" 추가

---
+++ LWBUI/LWBUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LWBUIApp.swift
+//  LWBUI
+//
+//  Created by 이원빈 on 2023/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LWBUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
