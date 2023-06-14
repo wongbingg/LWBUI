3e936864bb56c40634a447ff1d37292ba276ee1e
wonbeen <wb277@naver.com>
2023-06-14 09:30:27 +0900

    feat : "ContentView" 추가

---
+++ LWBUI/Sources/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  LWBUI
+//
+//  Created by 이원빈 on 2023/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var category: String = "포트폴리오"
+
+    var body: some View {
+//        ModernCategoryView(
+//            data: ["전체", "포트폴리오", "핀테크 트렌드", "핫 플레이스", "쿨 피플", "잘알못 칼럼"],
+//            selectedCategory: $category
+//        )
+        
+        TestView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
