84d913e9ee087eab90fe0c895fc3ea770baff227
WonbeenLee-bskr <wb277@naver.com>
2023-06-19 14:10:43 +0900

    style : 마크주석 추가

--- LWBUI/Sources/ContentView.swift
+++ LWBUI/Sources/ContentView.swift
@@ -11,11 +11,13 @@ struct ContentView: View {
     @State private var category: String = "포트폴리오"
 
     var body: some View {
+        // MARK: - Modern Category View
 //        ModernCategoryView(
 //            data: ["전체", "포트폴리오", "핀테크 트렌드", "핫 플레이스", "쿨 피플", "잘알못 칼럼"],
 //            selectedCategory: $category
 //        )
         
+        // MARK: - Bottom Sheet
         TestView()
     }
 }
